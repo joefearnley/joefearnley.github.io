e04b94c156172d459a61cb4d1fa8d48f6bfa2d0a
Joe Fearnley <joe.fearnley@gmail.com>
2018-05-11 18:17:30 -0400

    Setting up vue-cli

---
+++ src/components/Home.vue
@@ -0,0 +1,28 @@
+<template>
+<div id="wrapper">
+  <div id="main">
+    <header id="header">
+        <h1>Joe Fearnley</h1>
+        <p>Developer &nbsp;&bull;&nbsp; Pizza Eater &nbsp;&bull;&nbsp; Some Music</p>
+          <nav>
+          <ul>
+            <li><a href="https://github.com/joefearnley" class="icon fa-github"><span class="label">Github</span></a></li>
+            <li><a href="https://twitter.com/joefearnley" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+            <li><a href="https://instagram.com/joefearnley" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+            <li><a href="https://www.linkedin.com/in/joefearnley" class="icon fa-linkedin"><span class="label">LinkedIn</span></a></li>
+            <li><a href="mailto:joe.fearnley@gmail.com" class="icon fa-envelope"><span class="label">Email</span></a></li>
+          </ul>
+        </nav>
+    </header>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {}
+  }
+}
+</script>
