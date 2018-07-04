b0ee7ac17ffc0a3224044bfb004570a888c74bff
Joe Fearnley <joe.fearnley@gmail.com>
2018-07-04 10:31:41 -0400

    Adding email form to home component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -10,10 +10,42 @@
             <li><a href="https://twitter.com/joefearnley" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
             <li><a href="https://instagram.com/joefearnley" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
             <li><a href="https://www.linkedin.com/in/joefearnley" class="icon fa-linkedin"><span class="label">LinkedIn</span></a></li>
-            <li><a href="mailto:joe.fearnley@gmail.com" class="icon fa-envelope"><span class="label">Email</span></a></li>
+            <li><a @click="email()" href="#" class="icon fa-envelope"><span class="label">Email</span></a></li>
           </ul>
         </nav>
     </header>
+  </div>
+  <div class="modal" v-bind:class="{ 'is-active': showEmailForm }">
+    <div class="modal-background"></div>
+    <div class="modal-card">
+      <header class="modal-card-head">
+        <p class="modal-card-title">Email Joe</p>
+        <button @click="hideEmailForm" class="delete" aria-label="close"></button>
+      </header>
+      <section class="modal-card-body">
+        <form>
+          <div class="field">
+            <div class="control">
+              <input class="input" type="text" name="subject" placeholder="Subject">
+            </div>
+          </div>
+          <div class="field">
+            <div class="control">
+              <textarea class="textarea" name="body" placeholder="Hi Joe, ..."></textarea>
+            </div>
+          </div>
+        </form>
+      </section>
+      <footer class="modal-card-foot">
+        <button class="button is-success">
+          <span class="icon is-small">
+            <i class="fas fa-envelope"></i>
+          </span>
+          Send
+        </button>
+        <button @click="hideEmailForm" class="button">Cancel</button>
+      </footer>
+    </div>
   </div>
 </div>
 </template>
@@ -22,7 +54,17 @@
 export default {
   name: 'Home',
   data () {
-    return {}
+    return {
+      showEmailForm: false
+    }
+  },
+  methods: {
+    email: function() {
+      this.showEmailForm = true;
+    },
+    hideEmailForm: function() {
+      this.showEmailForm = false;
+    }
   }
 }
 </script>
