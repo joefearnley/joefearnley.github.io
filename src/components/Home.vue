20d2ceb528f966c349299358a37ea5a9ddd0814f
Joe Fearnley <joe.fearnley@gmail.com>
2018-07-06 16:10:54 -0400

    Adding form validation

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -10,12 +10,12 @@
             <li><a href="https://twitter.com/joefearnley" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
             <li><a href="https://instagram.com/joefearnley" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
             <li><a href="https://www.linkedin.com/in/joefearnley" class="icon fa-linkedin"><span class="label">LinkedIn</span></a></li>
-            <li><a @click="email()" href="#" class="icon fa-envelope"><span class="label">Email</span></a></li>
+            <li><a @click="showEmailForm()" href="#" class="icon fa-envelope"><span class="label">Email</span></a></li>
           </ul>
         </nav>
     </header>
   </div>
-  <div class="modal" v-bind:class="{ 'is-active': showEmailForm }">
+  <div class="modal" v-bind:class="{ 'is-active': showEmailFormModal }">
     <div class="modal-background"></div>
     <div class="modal-card">
       <header class="modal-card-head">
@@ -26,18 +26,32 @@
         <form>
           <div class="field">
             <div class="control">
-              <input class="input" type="text" name="subject" placeholder="Subject">
+              <input class="input" v-bind:class="{ 'is-danger': nameInvalid }" @change="validateField('name')" type="text" name="name" v-model="fields.name" placeholder="Name">
             </div>
+            <p v-show="nameInvalid" class="help is-danger">Name is required</p>
           </div>
           <div class="field">
             <div class="control">
-              <textarea class="textarea" name="body" placeholder="Hi Joe, ..."></textarea>
+              <input class="input" v-bind:class="{ 'is-danger': emailInvalid }" @change="validateField('email')" type="text" name="email" v-model="fields.email" placeholder="Email">
             </div>
+            <p v-show="emailInvalid" class="help is-danger">Email is required</p>
+          </div>
+          <div class="field">
+            <div class="control">
+              <input class="input" v-bind:class="{ 'is-danger': subjectInvalid }" @change="validateField('subject')" type="text" name="subject" v-model="fields.subject" placeholder="Subject">
+            </div>
+            <p v-show="subjectInvalid" class="help is-danger">Subject is required</p>
+          </div>
+          <div class="field">
+            <div class="control">
+              <textarea class="textarea" v-bind:class="{ 'is-danger': bodyInvalid }" @change="validateField('body')" name="body" v-model="fields.body" placeholder="Hi Joe, ..."></textarea>
+            </div>
+            <p v-show="bodyInvalid" class="help is-danger">Body is required</p>
           </div>
         </form>
       </section>
       <footer class="modal-card-foot">
-        <button class="button is-success">
+        <button @click="sendEmail" class="button is-success has-background-grey">
           <span class="icon is-small">
             <i class="fa fa-envelope"></i>
           </span>
@@ -55,15 +69,43 @@ export default {
   name: 'Home',
   data () {
     return {
-      showEmailForm: false
+      fields: {
+        name: '',
+        email: '',
+        subject: '',
+        body: ''
+      },
+      showEmailFormModal: false,
+      nameInvalid: false,
+      emailInvalid: false,
+      subjectInvalid: false,
+      bodyInvalid: false
     }
   },
   methods: {
-    email: function() {
-      this.showEmailForm = true;
+    showEmailForm: function() {
+      this.showEmailFormModal = true;
     },
     hideEmailForm: function() {
-      this.showEmailForm = false;
+      this.showEmailFormModal = false;
+    },
+    sendEmail: function() {
+      if (!this.validateForm()) {
+        return false;
+      }
+    },
+    validateForm: function() {
+      Object.keys(this.fields).forEach(key => {
+        this[key + 'Invalid'] = false;
+        if (this.fields[key] === '') {
+          this[key + 'Invalid'] = true;
+        }
+      });
+    },
+    validateField: function(key) {
+      if (this.fields[key] !== '') {
+        this[key + 'Invalid'] = false;
+      }
     }
   }
 }
